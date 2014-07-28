d7a2caf4c27c0aab106a03d4b879b627ff640e61
Julian Gruber <julian@juliangruber.com>
2014-07-28 08:52:05 +0200

    refactor like a champ

--- server.go
+++ server.go
@@ -1,115 +1,82 @@
-
 // TODO timeout based on querystring
 
 package main
 
 import (
-  "net/http"
-  "net/url"
-  "fmt"
-  "time"
-  "regexp"
-  "io/ioutil"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"regexp"
+	"time"
 )
 
-var green, yellow, red = func () ([]byte, []byte, []byte) {
-  green, err := ioutil.ReadFile("./images/green.gif")
-  if err != nil { panic(err) }
+var reg = regexp.MustCompile("^/http:\\/")
 
-  yellow, err := ioutil.ReadFile("./images/yellow.gif")
-  if err != nil { panic(err) }
+func getColor(url string, query url.Values) (string, error) {
+	start := time.Now()
+	resp, err := http.Get(url)
 
-  red, err := ioutil.ReadFile("./images/red.gif")
-  if err != nil { panic(err) }
+	if err != nil {
+		return "red", nil
+	}
 
-  return green, yellow, red
-}()
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
 
-var colors = map[string][]byte{
-  "green": green,
-  "yellow": yellow,
-  "red": red,
+	if err != nil {
+		return "red", nil
+	}
+
+	valid := true
+
+	if _, found := query["regex"]; found {
+		valid, err = regexp.Match(query["regex"][0], body)
+	} else if _, found := query["not-regex"]; found {
+		valid, err = regexp.Match(query["not-regex"][0], body)
+		valid = !valid
+	}
+
+	if err != nil {
+		return "", err
+	}
+
+	if !valid {
+		return "red", nil
+	}
+
+	if dt := time.Now().Sub(start); dt > time.Second {
+		return "yellow", nil
+	} else {
+		return "green", nil
+	}
 }
 
-var reg, _ = regexp.Compile("^/http:\\/")
+func newHandler() http.HandlerFunc {
+	colors := make(map[string][]byte)
+	colors["green"], _ = ioutil.ReadFile("images/green.gif")
+	colors["yellow"], _ = ioutil.ReadFile("images/yellow.gif")
+	colors["red"], _ = ioutil.ReadFile("images/red.gif")
 
-func testRegexp (r string, body []byte) (bool, error) {
-  return regexp.Match(r, body)
-}
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/favicon.ico" {
+			fmt.Fprintf(w, "404")
+			return
+		}
 
-func testNotRegexp (r string, body []byte) (bool, error) {
-  matched, err := regexp.Match(r, body)
-  return !matched, err
-}
+		url := "http://" + reg.ReplaceAllString(r.URL.Path, "")
+		query := r.URL.Query()
 
-func getColor(url string, query url.Values, c chan string, e chan error) {
-  start := time.Now()
-
-  resp, err := http.Get(url)
-
-  if err != nil {
-    c <- "red"
-    return
-  }
-
-  defer resp.Body.Close()
-  body, err := ioutil.ReadAll(resp.Body)
-
-  if err != nil {
-    c <- "red"
-    return
-  }
-
-  valid := true
-
-  if _, found := query["regex"]; found {
-    valid, err = testRegexp(query["regex"][0], body)
-  } else if _, found := query["not-regex"]; found {
-    valid, err = testNotRegexp(query["not-regex"][0], body)
-  }
-
-  if err != nil { e <- err }
-  if !valid {
-    c <- "red"
-    return
-  }
-
-  if dt := time.Now().Sub(start); dt > time.Second {
-    c <- "yellow"
-    return
-  } else {
-    c <- "green"
-    return
-  }
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-  if r.URL.Path == "/favicon.ico" {
-    fmt.Fprintf(w, "404")
-    return
-  }
-
-  url := "http://" + reg.ReplaceAllString(r.URL.Path, "")
-  query := r.URL.Query()
-
-  color := make(chan string)
-  err := make(chan error)
-
-  go getColor(url, query, color, err)
-
-  for {
-    select {
-      case c := <-color:
-        w.Write(colors[c])
-        return
-      case e := <-err:
-        fmt.Fprintf(w, e.Error())
-        return
-    }
-  }
+		color, err := getColor(url, query)
+		if err != nil {
+			fmt.Fprintf(w, err.Error())
+		} else {
+			w.Write(colors[color])
+		}
+	}
 }
 
 func main() {
-  http.HandleFunc("/", handler)
-  http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", newHandler())
+	http.ListenAndServe(":8080", nil)
 }
